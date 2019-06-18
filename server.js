7ce6b618a47619ba34f557b0496ec61e72b53eb6
Anna Langner <annalangner06@gmail.com>
2019-06-18 12:32:36 +0200

    configure the authorization request and create paths for the app

--- server.js
+++ server.js
@@ -14,19 +14,47 @@ passport.deserializeUser(function(obj, done) {
     done(null, obj);
 });
 
-app.use(express.static('css'));
-app.use(express.static('assets'));
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret:config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+app.use(express.static('css'));
+app.use(express.static('assets'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', { user: req.user });
 });
 
-app.get('/auth/google', function (req, res) {
-    res.render('google');
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope : ['profile', 'email']
+    }));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
 
 app.listen(3000);
 
