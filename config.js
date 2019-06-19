0c55f490593bf5d2b55346dfe211b974a5d46cca
Anna Langner <annalangner06@gmail.com>
2019-06-19 12:25:37 +0200

    API key change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'GOOGLE_CLIENT_ID': '990859328246-ssl9v63nir9q0cuvtntp8qhk9g0lemu3.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET': 'YN5hxB-x8bReZO8vLMhxFca7',
+    'GOOGLE_CLIENT_ID': '1032844402036-jqlef6u9ob7t77hro83i8t11ctnkv8ig.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'v3Q1TJ5UBpy4ImRgN9BW8ldK',
     'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
