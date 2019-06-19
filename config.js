972b4a0f3d03dcdd7d87ce9224dc95c174742361
Anna Langner <annalangner06@gmail.com>
2019-06-19 11:24:47 +0200

    typo improvement in the callback word

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'GOOGLE_CLIENT_ID': '990859328246-ssl9v63nir9q0cuvtntp8qhk9g0lemu3.apps.googleusercontent.com',
     'GOOGLE_CLIENT_SECRET': 'YN5hxB-x8bReZO8vLMhxFca7',
-    'CALBACK_URL': 'http://localhost:3000/auth/google/calback'
+    'CALBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
